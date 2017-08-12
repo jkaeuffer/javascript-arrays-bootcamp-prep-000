bc65c3c89f5ae429d60fe3731362504c0f7ab38c
Josephine Gavignet <josephinegavignet@gmail.com>
2017-08-12 00:21:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,6 +25,6 @@ function accessElementInArray(array, index) {
 };
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  array.shift(array[0])
+  array.shift()
   return array
-}
+};
