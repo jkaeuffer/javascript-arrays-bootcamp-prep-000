676f58c6f66376a7b27ec51810cbce6a9ae8a334
Josephine Gavignet <josephinegavignet@gmail.com>
2017-08-12 00:16:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,14 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
-  var newArray = [element, ... array]
+  var newArray = [element, ...array]
   return newArray
 };
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element)
+};
+
+function addElementToEndOfArray(array, element) {
+  var newArray = [...array, element]
+}
