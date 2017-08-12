48d9c453243871ce01ef213107a18f63d9f997f9
Josephine Gavignet <josephinegavignet@gmail.com>
2017-08-12 00:23:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -42,4 +42,4 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 function removeElementFromEndOfArray(array) {
   var newArray = array.slice(0, array.length - 1)
   return newArray
-}
+};
